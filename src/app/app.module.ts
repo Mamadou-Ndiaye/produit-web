31da4f7653ccc5cb89293ee967320ee161bdfa67
M ND <mamadou.ndiaye@free-partenaires.sn>
2022-12-07 08:44:27 +0000

    Update app 13 to 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,23 @@ import { FormsModule } from '@angular/forms';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
 import { RechercheParNomComponent } from './recherche-par-nom/recherche-par-nom.component';
+
+
+export  function kcFactory(kcService: KeycloakService){
+  return ()=>{
+      kcService.init({
+       config: {
+              realm : "product-realm",
+              clientId : "product-client",
+              url : "http://localhost:9090"
+             },
+             initOptions : {
+              onLoad : "login-required",
+              checkLoginIframe :true
+             }
+      })
+  }
+}
 
 @NgModule({
   declarations: [
@@ -24,9 +42,12 @@ import { RechercheParNomComponent } from './recherche-par-nom/recherche-par-nom.
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {provide : APP_INITIALIZER, deps : [KeycloakService], useFactory : kcFactory, multi : true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
