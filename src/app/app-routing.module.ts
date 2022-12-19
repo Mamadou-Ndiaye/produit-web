7de3ff1bd99a7a1c625c80873c20fd2a8b311f85
M ND <mamadou.ndiaye@free-partenaires.sn>
2022-12-19 16:31:38 +0000

    Proteger les routes avec les roles des user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
+import { AuthGuard } from './guard/security.guard';
 import { ProductsComponent } from './products/products.component';
 import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
 import { RechercheParNomComponent } from './recherche-par-nom/recherche-par-nom.component';
 
 const routes: Routes = [
   {path:  "products",    component: ProductsComponent},
-  {path:  "add-product", component: AddProductComponent},
-  {path:  "rechercheParCategorie", component: RechercheParCategorieComponent},
-  {path:  "rechercheParNom", component: RechercheParNomComponent},
-  {path:  "edit-product/:id", component: EditProductComponent },
+  {path:  "add-product", component: AddProductComponent, canActivate : [AuthGuard], data : { roles : ['ADMIN']}},
+  {path:  "rechercheParCategorie", component: RechercheParCategorieComponent, canActivate : [AuthGuard], data : { roles : ['USER']}},
+  {path:  "rechercheParNom", component: RechercheParNomComponent, canActivate : [AuthGuard], data : { roles : ['USER']}},
+  {path:  "edit-product/:id", component: EditProductComponent, canActivate : [AuthGuard], data : { roles : ['ADMIN']} },
   {path: "", redirectTo: "products", pathMatch: "full" }
 
 ];
